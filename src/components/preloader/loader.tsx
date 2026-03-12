b1e8db0a285ad20137a8b4f9908d897752b13b93
Parthiban A <partha@2003>
2026-03-12 17:52:32 +0530

    again deploy issue5

--- src/components/preloader/loader.tsx
+++ src/components/preloader/loader.tsx
@@ -5,37 +5,13 @@ import { motion } from "framer-motion";
 import { opacity, slideUp } from "./anim";
 import { usePreloader } from ".";
 
-const steps = [
-  "10%",
-  "20%",
-  "30%",
-  "40%",
-  "50%",
-  "60%",
-  "70%",
-  "80%",
-  "90%",
-  "100%",
-];
-
-export default function Index() {
-  const { isLoading, loadingPercent } = usePreloader();
-  const [index, setIndex] = useState(0);
+export default function Loader() {
+  const { loadingPercent } = usePreloader();
   const [dimension, setDimension] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
     setDimension({ width: window.innerWidth, height: window.innerHeight });
   }, []);
-
-  useEffect(() => {
-    if (index == steps.length - 1) return;
-    setTimeout(
-      () => {
-        setIndex(index + 1);
-      },
-      index == 0 ? 1000 : 150
-    );
-  }, [index]);
 
   const initialPath = `M0 0 L${dimension.width} 0 L${dimension.width} ${
     dimension.height
@@ -67,7 +43,7 @@ export default function Index() {
       {dimension.width > 0 && (
         <>
           <motion.p variants={opacity} initial="initial" animate="enter">
-            {(loadingPercent - (loadingPercent % 5)).toFixed(0)} %
+            {Math.round(loadingPercent)}%
           </motion.p>
           <svg>
             <motion.path
